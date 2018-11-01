f827bfc0226e630497e7df6727bf589cf479465c
BoraQu <348053718@qq.com>
2018-11-01 19:46:11 +0800

    添加person

---
+++ Test/Test/XLPerson.h
@@ -0,0 +1,17 @@
+//
+//  XLPerson.h
+//  Test
+//
+//  Created by Lei Xu on 2018/11/1.
+//  Copyright © 2018 rcoming. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XLPerson : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Test/Test/XLPerson.m
@@ -0,0 +1,13 @@
+//
+//  XLPerson.m
+//  Test
+//
+//  Created by Lei Xu on 2018/11/1.
+//  Copyright © 2018 rcoming. All rights reserved.
+//
+
+#import "XLPerson.h"
+
+@implementation XLPerson
+
+@end
